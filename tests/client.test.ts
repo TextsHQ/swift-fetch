a2d81239d2b0bf7df5c48e01d2a7fbd678f6af4e
1Conan <me@1conan.com>
2023-04-29 07:20:50 +0800

    add test

---
+++ tests/client.test.ts
@@ -0,0 +1,9 @@
+import SwiftFetch from '../dist'
+
+test('Fetch JSON document', async () => {
+  const response = await SwiftFetch.requestAsString('https://httpbin.org/json')
+
+  expect(response.statusCode).toBe(200)
+  expect(response.body).toBeDefined()
+  expect(JSON.parse(response.body)).toBeDefined()
+})
