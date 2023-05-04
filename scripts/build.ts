8c3ef87362b1808440ec4418f2e8fd64b1a9b005
1Conan <me@1conan.com>
2023-05-04 17:39:27 +0800

    linux size optimization

--- scripts/build.ts
+++ scripts/build.ts
@@ -21,17 +21,25 @@ async function runAndCapture(command: string, args: readonly string[], options: 
 (async () => {
   const swiftFlags = [] as unknown as [string]
   const cFlags = [] as unknown as [string]
+  const linkerFlags = [] as unknown as [string]
 
-  if (os.platform() === 'darwin') {
+  const osPlatform = os.platform()
+
+  if (osPlatform === 'darwin') {
     const platform = process.env.NODESWIFT_PLATFORM || 'macosx'
     const target = process.env.NODESWIFT_TARGET || 'arm64-apple-macosx10.15'
     const sdkPath = await (await runAndCapture('xcrun', ['-sdk', platform, '-show-sdk-path'])).trimEnd()
     swiftFlags.push('-sdk', sdkPath, '-target', target)
     cFlags.push('-isysroot', sdkPath, '-target', target)
+  } else if (osPlatform === 'linux') {
+    cFlags.push('-Os', '-ffunction-sections', '-fdata-sections')
+    swiftFlags.push('-Osize', '-wmo')
+    linkerFlags.push('--gc-sections')
   }
 
   await build(isDebug ? 'debug' : 'release', {
     swiftFlags,
     cFlags,
+    linkerFlags,
   })
 })()
