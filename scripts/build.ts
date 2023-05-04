1eac8d092b133dbdc5cbeb580a6750c1ede63d86
1Conan <me@1conan.com>
2023-05-04 11:37:06 +0800

    only add macos flags on macos

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,10 +1,8 @@
 import { spawn, SpawnOptions } from 'child_process'
+import os from 'os'
 import { build } from 'node-swift'
 
 const isDebug = process.env.NODE_ENV === 'development' || process.argv.includes('--debug')
-
-const platform = process.env.NODESWIFT_PLATFORM || 'macosx'
-const target = process.env.NODESWIFT_TARGET || 'arm64-apple-macosx10.15'
 
 async function runAndCapture(command: string, args: readonly string[], options: SpawnOptions = {}): Promise<string> {
   return new Promise((res, rej) => {
@@ -21,18 +19,19 @@ async function runAndCapture(command: string, args: readonly string[], options: 
 }
 
 (async () => {
-  const sdkPath = await (await runAndCapture('xcrun', ['-sdk', platform, '-show-sdk-path'])).trimEnd()
+  const swiftFlags = [] as unknown as [string]
+  const cFlags = [] as unknown as [string]
+
+  if (os.platform() === 'darwin') {
+    const platform = process.env.NODESWIFT_PLATFORM || 'macosx'
+    const target = process.env.NODESWIFT_TARGET || 'arm64-apple-macosx10.15'
+    const sdkPath = await (await runAndCapture('xcrun', ['-sdk', platform, '-show-sdk-path'])).trimEnd()
+    swiftFlags.push('-sdk', sdkPath, '-target', target)
+    cFlags.push('-isysroot', sdkPath, '-target', target)
+  }
 
   await build(isDebug ? 'debug' : 'release', {
-    swiftFlags: [
-      '-sdk', sdkPath,
-      '-target', target,
-    ],
-    cFlags: [
-      '-isysroot', sdkPath,
-      '-target', target,
-    ],
-    napi: 7,
-    enableEvolution: true,
+    swiftFlags,
+    cFlags,
   })
 })()
